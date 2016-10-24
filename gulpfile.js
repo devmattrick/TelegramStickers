182a075e2df6825f083e14a4f615ca02fd5ce8ad
mattrick <social@mattrick.me>
2016-10-23 18:16:50 -0700

    Add gulp-changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ const gulp = require("gulp");
 const gm = require('gulp-gm');
 const imagemin = require("gulp-imagemin");
 const watch = require("gulp-watch");
+const changed = require('gulp-changed');
 
 gulp.task('default', function() {
 	//Watch for file changes
 	gulp.src('src/*')
+	//Ensure files that are the same don't get re-converted
+	.pipe(changed('dist'))
 	.pipe(gm(function (gmfile) {
 		//Resize largest side to 512px (Telegram's requirement), maintaining aspect ratio (also convert to PNG)
 		return gmfile.resize(512, 512).setFormat('png');
